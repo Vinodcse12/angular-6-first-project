cb11db635da221c300d4e26613d8952a82f07707
Vinod Sharma <vinod.sharma@powerschool.com>
2018-09-25 10:34:17 +0530

    routing

--- firstapp/src/app/app.module.ts
+++ firstapp/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyContainerComponent } from './components/body-container/body-container.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+const route : Routes = [
+  { path : 'home', component : HomeComponent },
+  { path : 'about', component : AboutComponent} 
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyContainerComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
